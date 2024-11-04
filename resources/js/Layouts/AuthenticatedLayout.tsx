63b500fc943e21337a9243edc68640b2362fbc04
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2024-11-04 21:16:46 +0400

    Implement users listing and role changing

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -4,6 +4,7 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import {Link, usePage} from '@inertiajs/react';
 import {PropsWithChildren, ReactNode, useState} from 'react';
+import {can, hasRole} from "@/helpers";
 
 export default function Authenticated({
                                         header,
@@ -40,6 +41,12 @@ export default function Authenticated({
                 >
                   Features
                 </NavLink>
+                {can(user, 'manage_users') && <NavLink
+                  href={route('user.index')}
+                  active={route().current('user.index')}
+                >
+                  Users
+                </NavLink>}
               </div>
             </div>
 
@@ -150,6 +157,12 @@ export default function Authenticated({
             >
               Features
             </ResponsiveNavLink>
+            {can(user, 'manage_users') && <ResponsiveNavLink
+              href={route('user.index')}
+              active={route().current('user.index')}
+            >
+              Users
+            </ResponsiveNavLink>}
           </div>
 
           <div className="border-t border-gray-200 pb-1 pt-4 dark:border-gray-600">
